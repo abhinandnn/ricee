872793b6a049b509e45208479001c1fe42689a11
Somgupta786 <somvishwari786@gmail.com>
2024-01-23 02:26:57 +0530

    pull the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
+import hamburgerimg from '../../public/hamburger.svg'
+import Image from 'next/image'
 function Header() {
+        const[hamburger,setHamburger]=useState(false);
   return (
-    <div className='w-screen absolute backdrop-blur-[10px] z-10'>
-    <div className='bg-black bg-opacity-20 text-[1.25rem] text-white h-[4.5rem] flex items-center justify-center relative'>
-    <div className=' fixed left-[6.5rem]'>Ricee</div>
-    <div className='flex font-gsmt text-[1.0625rem] gap-[3rem]'>
+    <div className='w-screen absolute z-10 '>
+    <div className='bg-black backdrop-blur-[10px] bg-opacity-20 text-[1.25rem] text-white px-4 h-[4.5rem] flex items-center sm:justify-end justify-center relative'>
+    <div className=' fixed left-[6.5rem] lg:left-[5rem] sm:text-[1.75rem] mob:left-[1rem] md:left-[2rem]'>Ricee</div>
+    <div className='flex font-gsmt text-[1.0625rem] sm:hidden gap-[3rem]'>
         <div> 
 About
         </div>
@@ -16,6 +19,18 @@ Location
 Products
         </div>
     </div>
+<Image onClick={()=>setHamburger(!hamburger)} className='hidden sm:inline cursor-pointer' src={hamburgerimg}/>
+    </div>
+    <div className={`hidden sm:${hamburger ? 'flex' : 'hidden'} w-[10rem] gap-2 bg-white text-[1.25rem] flex-col absolute right-0`}>
+    <div className='py-[0.3rem] pl-[1rem]'> 
+About
+        </div>
+        <div className='py-[0.3rem] pl-[1rem]'>
+Location
+        </div>
+        <div className='py-[0.3rem] pl-[1rem]'>
+Products
+        </div>
     </div>
     </div>
   )
--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -3,9 +3,9 @@ import Header from './Header'
 import background from '../../public/topBanner.png'
 function TopBanner() {
   return (
-    <div className="relative h-[41.5rem] bg-cover" style={{backgroundImage: `url(${background.src})`  }}>
-      <div className='text-[3.5rem] text-white min-w-[49rem] w-[54rem] px-[6.5rem] pt-[9.4rem]'>Grains of Goodness: Elevate Your Plate with Our Premium Rice Selection!</div>
-      <button className={`w-[13.875rem] h-[4rem] bg-mar rounded-[0.75rem] text-[1rem] text-white mx-[6.5rem] mt-[6rem]`}>Order now</button>
+    <div className="relative h-auto pb-[4rem] bg-cover bg-center" style={{backgroundImage: `url(${background.src})`  }}>
+      <div className='text-[3.5rem] md:pt-[7rem] mob:pt-[5rem] lg:text-[2.8rem]  sm:text-[2rem] sm:w-[68vw] mini:w-[80vw] lg:w-[40rem] mob:pl-[1rem] mob:pr-0 md:w-[36rem] sm:leading-[2.5rem] md:pl-8 sm:pr-0 leading-[4.2rem] text-white w-[54rem] px-[6.5rem] lg:px-[5rem]  pt-[9.4rem]'>Grains of Goodness: Elevate Your Plate with Our Premium Rice Selection!</div>
+      <button className={`w-[13.875rem] h-[4rem] mob:absolute bottom-[-7%] sm:w-[10rem] sm:h-[3.6rem] bg-mar rounded-[0.75rem] mob:ml-[1rem] text-[1rem] md:mx-8 md:mt-[3rem] text-white lg:mx-[5rem] mx-[6.5rem] mt-[5rem]`}>Order now</button>
     </div>
   )
 }
