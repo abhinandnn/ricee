f1c4c614894ef895e28614eb6c90aa861b91be84
Abhinandan <abhinandnn21@gmail.com>
2024-01-23 04:01:24 +0530

    linked the navbar and fixed bugs in css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,35 +2,36 @@
 import React, { useState } from 'react'
 import hamburgerimg from '../../public/hamburger.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 function Header() {
         const[hamburger,setHamburger]=useState(false);
   return (
-    <div className='w-screen absolute z-10 '>
+    <div className='w-screen fixed z-10 '>
     <div className='bg-black backdrop-blur-[10px] bg-opacity-20 text-[1.25rem] text-white px-4 h-[4.5rem] flex items-center sm:justify-end justify-center relative'>
     <div className=' fixed left-[6.5rem] lg:left-[5rem] sm:text-[1.75rem] mob:left-[1rem] md:left-[2rem]'>Ricee</div>
     <div className='flex font-gsmt text-[1.0625rem] sm:hidden gap-[3rem]'>
-        <div> 
+        <Link href="#about" >
 About
-        </div>
-        <div>
+</Link>
+        <Link href={'#location'}>
 Location
-        </div>
-        <div>
+</Link>
+<Link href={'#products'}>
 Products
-        </div>
+</Link>
     </div>
 <Image onClick={()=>setHamburger(!hamburger)} className='hidden sm:inline cursor-pointer' src={hamburgerimg}/>
     </div>
     <div className={`hidden sm:${hamburger ? 'flex' : 'hidden'} w-[10rem] gap-2 bg-white text-[1.25rem] flex-col absolute right-0`}>
-    <div className='py-[0.3rem] pl-[1rem]'> 
+    <Link onClick={()=>setHamburger(!hamburger)} href="#about" className='py-[0.3rem] pl-[1rem]'>
 About
-        </div>
-        <div className='py-[0.3rem] pl-[1rem]'>
+</Link>
+        <Link onClick={()=>setHamburger(!hamburger)} href={'#location'} className='py-[0.3rem] pl-[1rem]'>
 Location
-        </div>
-        <div className='py-[0.3rem] pl-[1rem]'>
+</Link>
+<Link onClick={()=>setHamburger(!hamburger)} href={'#products'} className='py-[0.3rem] pl-[1rem]'>
 Products
-        </div>
+</Link>
     </div>
     </div>
   )
