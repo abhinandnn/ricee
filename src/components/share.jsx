af95e5ed02afc32eabf79b0c5a33c64d1e17bc1e
Somgupta786 <somvishwari786@gmail.com>
2024-01-25 03:00:58 +0530

    fix cursor bug

--- src/components/share.jsx
+++ src/components/share.jsx
@@ -7,9 +7,9 @@ const Share = () => {
  <img src="handss.png " className=" absolute top-[50%] translate-y-[-50%] right-0   md1:w-[63.3vw] mob:right-[-5vw] mob:w-[217px]" />
       <div className=" w-[52.08vw] flex flex-col gap-[4.2vh] md:w-[66vw]">
         <div className=" font-gd text-[3rem] not-italic font-[400] leading-[61.44px] text-[#0F0D0A] mob1:text-[2rem] mob1:leading-[40.96px] ">Connect now with our experts for business enquiries</div>
-        <div className="btn w-[205px] h-[64px] mob1:w-[165px] mob1:h-[56px] rounded-[12px] bg-[#762023] flex justify-center items-center gap-[12px]">
+        <div className="btn z-10 cursor-pointer w-[205px] h-[64px] mob1:w-[165px] mob1:h-[56px] rounded-[12px] bg-[#762023] flex justify-center items-center gap-[12px]">
             <img src="logo.svg"></img>
-            <div className=" text-[#FFF] font-[400] font-gd leading-[19.2px] tracking-[0.48px] not-italic text-[1rem] cursor-pointer">Whatsapp Now</div>
+            <div className=" text-[#FFF] font-[400] font-gd leading-[19.2px] tracking-[0.48px] not-italic text-[1rem] ">Whatsapp Now</div>
         </div>
       </div>
     </div>
