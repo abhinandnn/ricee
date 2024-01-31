e9dba2d5b55467498c7be279cd657c5a0ba46ae6
Somgupta786 <somvishwari786@gmail.com>
2024-01-31 22:57:04 +0530

    add whatsapp now button

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 
+
 const Footer = () => {
   return (
-
-    <div id="location" className=" relative flex justify-between mt-[24.3vh] mob1:mt-[17.9vh] w-[100vw]  bg-[#060606] text-[#FFF] pt-[6.3vh] pr-[9.09vw] pl-[7.22VW] text-[1.25rem] font-gd font-[400] leading-[30px] tracking-[0.6px] not-italic overflow-hidden  whitespace-nowrap  md:flex-col md:gap-[80px] pb-[7.3vh]  mob1:text-[16px]">
-
-    
-
+    <div
+      id="location"
+      className=" relative flex justify-between mt-[24.3vh] mob1:mt-[17.9vh] w-[100vw]  bg-[#060606] text-[#FFF] pt-[6.3vh] pr-[9.09vw] pl-[7.22VW] text-[1.25rem] font-gd font-[400] leading-[30px] tracking-[0.6px] not-italic overflow-hidden  whitespace-nowrap  md:flex-col md:gap-[80px] pb-[7.3vh]  mob1:text-[16px]"
+    >
       <div className=" flex flex-col gap-[48px] ">
         <div className=" flex flex-col gap-[24px]">
           <div className=" text-[2rem] leading-[48px] tracking-[0.96px] mob1:text-[28px]">
             Ricee
           </div>
-          <div>Our Rice, a Feast of Nutrients, <span className=" md1:block">a Banquet of Wellness.</span> </div>
+          <div>
+            Our Rice, a Feast of Nutrients,{" "}
+            <span className=" md1:block">a Banquet of Wellness.</span>{" "}
+          </div>
         </div>
         <div className=" flex flex-col gap-[12px]">
           <div>
             <img className=" inline" src="Location.svg" />
-            &nbsp;&nbsp;Lumbini Road, Kakrahwa Bazar, <span className=" block">&nbsp;&nbsp;&nbsp;&nbsp; Siddharthnagar, Uttar Pradesh, India</span>
+            &nbsp;&nbsp;Lumbini Road, Kakrahwa Bazar,{" "}
+            <span className=" block">
+              &nbsp;&nbsp;&nbsp;&nbsp; Siddharthnagar, Uttar Pradesh, India
+            </span>
           </div>
-         
         </div>
       </div>
       <div className=" flex flex-col gap-[32px] ">
         <div className=" mob1: text-[20px]">Contact Details</div>
         <div className=" flex flex-col gap-[2.1vh]">
-          <div className=" whitespace-nowrap">
-            <img className=" inline" src="Phone.svg" />
-            &nbsp;&nbsp;+91 9087621517
-          </div>
           <div className=" whitespace-nowrap flex">
             <img className=" inline" src="Phone.svg" />
             &nbsp;&nbsp;+91 9540182525
@@ -46,11 +47,15 @@ const Footer = () => {
         </div>
       </div>
       <div className="bgHold relative">
-        <img className="absolute mini:left-[-7rem] mob:left-[-5.5rem] sm:left-[-20%] md:left-[-14%] xl:left-[-6%]" src="rangoli.png" />
+        <img
+          className="absolute mini:left-[-7rem] mob:left-[-5.5rem] sm:left-[-20%] md:left-[-14%] xl:left-[-6%]"
+          src="rangoli.png"
+        />
       </div>
-      <div className=" absolute bottom-[2px] left-[50%] translate-x-[-50%] text-[1rem] text-[#5d5c5c] mob:text-[0.7rem]">Copyright &copy; 2022 P&B Agro Foods-All Rights Reserved</div>
+      <div className=" absolute bottom-[2px] left-[50%] translate-x-[-50%] text-[1rem] text-[#5d5c5c] mob:text-[0.7rem]">
+        Copyright &copy; 2022 P&B Agro Foods-All Rights Reserved
       </div>
-    
+    </div>
   );
 };
 
--- src/components/share.jsx
+++ src/components/share.jsx
@@ -1,5 +1,6 @@
+"use client"
 import React from "react";
-
+import ReactWhatsapp from 'react-whatsapp';
 const Share = () => {
   return (
     <div className="relative w-[100vw] pb-[12.56vh] border-t-[1px] border-b-[1px] border-solid border-[#744150] bg-[#FFE6CE] pl-[7.43vw] pt-[10.2vh] md:pt-[5.89vh] md:pl-[4.44vw] md:pb-[6.9vh]  overflow-hidden " >
@@ -7,10 +8,11 @@ const Share = () => {
  <img src="handss.png " className=" absolute top-[50%] translate-y-[-50%] right-0   md1:w-[63.3vw] mob:right-[-5vw] mob:w-[217px]" />
       <div className=" w-[52.08vw] flex flex-col gap-[4.2vh] md:w-[66vw]">
         <div className=" font-gd text-[3rem] not-italic font-[400] leading-[61.44px] text-[#0F0D0A] mob1:text-[2rem] mob1:leading-[40.96px] z-10 ">Connect now with our experts for business enquiries</div>
-        <button className="z-10 text-[#FFF] transition-hover duration-300 hover:bg-white hover:text-mar border border-mar w-[10.75rem] h-[3.6875rem]  mob:h-[56px] mob:w-[10rem] mob:gap-[8px]   rounded-[12px] bg-[#762023] flex justify-center items-center gap-[12px]">
+        <button onClick={()=>typeof window !== 'undefined'?window.open('https://web.whatsapp.com://send?text=WhatsApp now to order products'):null} className="z-10 text-[#FFF] transition-hover duration-300 hover:bg-white hover:text-mar border border-mar w-[10.75rem] h-[3.6875rem]  mob:h-[56px] mob:w-[10rem] mob:gap-[8px]   rounded-[12px] bg-[#762023] flex justify-center items-center gap-[12px]">
             <img src="logo.svg"></img>
             <div className="btn  font-[400] font-gd leading-[19.2px] tracking-[0.48px] not-italic text-[1rem] cursor-pointer whitespace-nowrap">Whatsapp Now</div>
         </button>
+        
       </div>
     </div>
   );
